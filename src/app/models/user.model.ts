4700043e4c035a55943b9bacb6d37d22a3e5ad2b
IgorGolubovski01 <igor.golubovski.21@singimail.rs>
2025-08-04 21:00:34 +0200

    .

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,3 +1,13 @@
+import { RoleModel } from "./role.model";
+
 export interface UserModel{
-    
+    id: number,
+    address: string,
+    email: string,
+    firstName: string,
+    lastName: string,
+    password: string,
+    phone: string,
+    username: string,
+    role: RoleModel
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,20 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { UserModel } from '../models/user.model';
 
 @Component({
   selector: 'app-signup',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
 
-  fistName: string|null = null
-  lastName: string|null = null
-  email: string|null = null
-  phone: string|null = null
-  password: string|null = null
-  rePassword: string|null = null
+  fistName: string = ''
+  lastName: string = ''
+  email: string = ''
+  phone: string = ''
+  password: string = ''
+  rePassword: string = ''
 
   constructor(){}
 }
